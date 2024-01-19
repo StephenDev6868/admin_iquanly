d3e048223ddce6ef3ccb9c3e898e44ca28eadd1c
Thanh Nguyen <thanh-nc@mcsolutions.vn>
2024-01-20 01:51:23 +0700

    update

--- backend/app/Helpers/helpers.php
+++ backend/app/Helpers/helpers.php
@@ -71,11 +71,11 @@ if (! function_exists('count_material_for_order')) {
                     $ingredient_item[] = [
                         'name' => $material->name,
                         'code' => $material->code,
-                        'quantity' => number_format((($value['amount']  * $value2['quota'] ) / $material->num_quota) * $lossConfig, '2', '.', ''),
+                        //'quantity' => number_format((($value['amount']  * $value2['quota'] ) / $material->num_quota) * $lossConfig, '2', '.', ''),
                         'unit' => $material->unit,
                         'size' => $product->size,
                     ];
-                    if ($data['id'] == '2') {
+                    if ($data['id'] == '3') {
                         dd($value['amount'], $value2['quota'], $material->num_quota, $lossConfig,
                             number_format((($value['amount']  * $value2['quota'] ) / $material->num_quota) * $lossConfig, '2', '.', '')
                         );
